d4552e25cd2b30ac6f2dbdfe60abc12e4e607eac
Jack Shen <shenpinyi@gmail.com>
2017-06-01 21:26:52 +1000

    initial

--- src/main/java/com/shenpinyi/other/codility/question1/Solution.java
+++ src/main/java/com/shenpinyi/other/codility/question1/Solution.java
@@ -5,15 +5,33 @@ package com.shenpinyi.other.codility.question1;
  */
 public class Solution {
 
-    public int solution() {
-        return 1;
+    int solution(int M, int[] A) {
+        int N = A.length;
+        int[] count = new int[M + 1];
+        for (int i = 0; i <= M; i++)
+            count[i] = 0;
+        int maxOccurence = 1;
+        int index = -1;
+        for (int i = 0; i < N; i++) {
+            if (count[A[i]] > 0) {
+                int tmp = count[A[i]];
+                if (tmp > maxOccurence) {
+                    maxOccurence = tmp;
+                    index = i;
+                }
+                count[A[i]] = tmp + 1;
+            } else {
+                count[A[i]] = 1;
+            }
+        }
+        return A[index];
     }
 
 
     public static void main(String[] args) {
         System.out.println(
             new Solution().
-                solution()
+                solution(3, new int[] {1,2,3,3,1,3,1})
         );
     }
 
