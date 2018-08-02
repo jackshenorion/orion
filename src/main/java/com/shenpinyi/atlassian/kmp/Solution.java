18661fb25a341799b41aad8285f827e82a88897f
Jack Shen <jack.shen@nasdaq.com>
2018-08-02 17:20:11 +1000

    lorica problem

---
+++ src/main/java/com/shenpinyi/atlassian/kmp/Solution.java
@@ -0,0 +1,13 @@
+package com.shenpinyi.atlassian.kmp;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().run("", ""));
+    }
+
+    private int run(String text, String word) {
+        return 0;
+    }
+
+}
---
+++ src/main/java/com/shenpinyi/cracking/linkedList/question2_2/Solution.java
@@ -0,0 +1,17 @@
+package com.shenpinyi.cracking.linkedList.question2_2;
+
+import java.util.LinkedList;
+
+public class Solution {
+
+
+
+    public static void main(String[] args) {
+
+        LinkedList<Object> list = new LinkedList<>();
+        list.add(1);
+        list.getFirst();
+
+    }
+
+}
