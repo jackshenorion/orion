57fd0021bfde2896ad7cce9d4fadca5080c4a598
Jack Shen <jack.shen@nasdaq.com>
2018-09-25 17:03:26 +1000

    add few questions

---
+++ src/test/java/com/shenpinyi/lintcode/graph/question433/SolutionTest.java
@@ -0,0 +1,14 @@
+package com.shenpinyi.lintcode.graph.question433;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void numIslands() {
+
+        new Solution().numIslands(new boolean[][]{{false},{false},{false},{false},{false}});
+    }
+}
